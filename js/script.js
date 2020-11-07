8764cb226c2c33aa01b4116399fcc085d2a6815a
Pawel Wisniewski <pawel_wisniewski@outlook.com>
2020-11-07 15:49:38 +0100

    Add result script

--- js/script.js
+++ js/script.js
@@ -33,3 +33,37 @@ if(playerInput == '3'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Ty przegrywasz!');
+}
+
+if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
+if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ty przegrywasz!');
+}
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+
+if( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Ty przegrywasz!');
+}
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+}
+
+if( playerMove == 'nieznany ruch'){
+  printMessage('OSZUST!!!')
+}
